e2ff01825de6c503e96bec502895c41e3e521293
MSRbuddy <meghanadasari222@gmail.com>
2023-04-01 16:31:38 +0530

    Added : Line-Cmprsn Prblm UC-1!

---
+++ LC-UC1/LC-UC1/LC_UC1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LC_UC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, x2, y1, y2;
+            x1 = 4;
+            x2 = 8;
+            y1 = 3;
+            y2 = 6;
+            var distance = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            Console.WriteLine(distance);
+        }
+    }
+}
