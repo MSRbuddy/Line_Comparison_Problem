9cbea75da2a8ab90d7786cbdb7aebae384e41061
MSRbuddy <meghanadasari222@gmail.com>
2023-04-01 16:33:05 +0530

    Added : Line-Cmprsn Prblm UC-2!

---
+++ LC-UC2/LC-UC2/LC-UC2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LC_UC2
+{
+    internal class program
+    { 
+        static void idstrt(double a1,double b1,double c1,double a2,double b2,double c2)
+        {
+            if (((a1 / a2).Equals(b1 / b2)) && ((a1 / a2).Equals(c1 / c2)) && ((b1 / b2).Equals(c1 / c2)))
+                Console.WriteLine("The given straight lines are equal");
+            else
+                Console.WriteLine("The given straight lines are not equal");
+        }
+        public static void Main(string[] args)
+        {
+            double a1 = 2, b1 = -4, c1 = 3, a2 = 6, b2 = -12, c2 = 9;
+            idstrt(a1, b1, c1, a2, b2, c2);
+        }
+    }
+}
